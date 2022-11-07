b1e587eaf7295c2b827c66ae70fc38be660a317d
Gabriel Leite <gabriel.leite@forma.arq>
2022-11-07 16:51:12 -0300

    alterações até capitulo 4

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -6,22 +6,19 @@ use Illuminate\Http\Request;
 
 class SeriesController extends Controller
 {
-    public function listarSeries()
+    public function index(Request $request)
     {
-        $series = [ 
+        $series = [
             'Punisher',
             'Lost',
-            'Grey\'s Anatomy'
+            'Grey\'s Anatomy',
         ];
 
-        $html = '<ul>';
+        return view('series.index')->with('series', $series);
+    }
 
-        foreach ($series as $serie) {
-            $html .= "<li>$serie</li>";
-        }
-
-        $html .= '</ul>';
-
-        echo $html;
+    public function create()
+    {
+        return view('series.create');
     }
 }
