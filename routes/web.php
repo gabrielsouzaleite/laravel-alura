6d7de0c2bd2a7be61cb3735c7501a763b8f43582
Gabriel Leite <gabriel.leite@forma.arq>
2022-11-08 16:09:53 -0300

    Finalização do primeiro curso de Laravel

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/series', [SeriesController::class, 'listarSeries']);
+Route::get('/series', [SeriesController::class, 'index']);
+Route::get('/series/criar', [SeriesController::class, 'create']);
+Route::post('/series/salvar', [SeriesController::class, 'store']);
